a6a06dd3a47882eaa15d5ebc787267edd3798c65
csurya89 <suryaakumarrsc@gmail.com>
2017-01-18 09:58:52 +0530

    Add files via upload

--- todo_task.sql
+++ todo_task.sql
@@ -2,8 +2,8 @@
 -- version 4.6.5.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 17, 2017 at 07:09 PM
+-- Host: localhost:3306
+-- Generation Time: Jan 18, 2017 at 04:27 AM
 -- Server version: 10.1.20-MariaDB
 -- PHP Version: 7.0.8
 
@@ -42,13 +42,6 @@ CREATE TABLE `users` (
   `modified` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `oauth_provider`, `oauth_uid`, `first_name`, `last_name`, `email`, `phone_no`, `gender`, `locale`, `picture_url`, `profile_url`, `created`, `modified`) VALUES
-(1, 'facebook', '1322862497752343', 'Surya', 'Kumar', 'suryaakumarrsc@gmail.com', NULL, 'female', 'en_US', 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/15578565_1294449953926931_5059806156410377839_n.jpg?oh=f71fda97b1ba23be12538308d96da021&oe=58DD3127', 'https://www.facebook.com/1322862497752343', 1484679990, 1484679990);
-
 -- --------------------------------------------------------
 
 --
@@ -63,16 +56,6 @@ CREATE TABLE `user_todo_tasks` (
   `todo_task_completed` int(11) NOT NULL DEFAULT '0',
   `todo_task_deleted` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_todo_tasks`
---
-
-INSERT INTO `user_todo_tasks` (`todo_task_id`, `todo_task_user_id`, `todo_task_name`, `todo_task_added`, `todo_task_completed`, `todo_task_deleted`) VALUES
-(1, 1, 'Banana', 1484680148, 0, 0),
-(2, 1, 'Apple', 1484680154, 0, 0),
-(3, 1, 'Orange', 1484680164, 1484680173, 0),
-(4, 1, 'Grapes', 1484680171, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -101,12 +84,12 @@ ALTER TABLE `user_todo_tasks`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user_todo_tasks`
 --
 ALTER TABLE `user_todo_tasks`
-  MODIFY `todo_task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `todo_task_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
